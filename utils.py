103a5fbc4bad34670fb3d645287b1580cb4a82c7
Lucio Messina <luciotuttominuscolo@inventati.org>
2020-07-29 18:12:22 +0200

    Farm: move run->map; implement map_reduce; remove Pipeline; remove utils

--- utils.py
+++
@@ -1,9 +0,0 @@
-def grouper (iterable, n):
-    out = []
-    for x in iterable:
-        out.append (x)
-        if len(out) == n:
-            yield out
-            out = []
-    if out:
-        yield out
