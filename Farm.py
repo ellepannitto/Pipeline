c9ae7c66c4c0ab2d71fa482a4e4bc011b520c5ae
Lucio Messina <luciotuttominuscolo@inventati.org>
2020-07-29 18:12:17 +0200

    Farm: move run->map; implement map_reduce; remove Pipeline; remove utils

--- Farm.py
+++ Farm.py
@@ -1,8 +1,11 @@
+import functools
 import logging
+from multiprocessing.pool import AsyncResult
 import tqdm
 import multiprocessing as mp
+from multiprocessing import TimeoutError
 
-import math
+from typing import List
 
 logger = logging.getLogger(__name__)
 
@@ -17,14 +20,17 @@ def grouper (iterable, n):
     if out:
         yield out
 
+def wrap_function_result_inside_a_list ( function, input ):
+    return [function(input)]
 
 class Farm:
 
-    def __init__(self, list_of_functions, nworkers, batchsize=1, show_progress_bar=True):
+    def __init__(self, list_of_functions, nworkers, batchsize=1, show_progress_bar=True, timeout=0.001):
         self.functions = list_of_functions
         self.nworkers = nworkers
         self.show_progress_bar = show_progress_bar
         self.batchsize = batchsize
+        self.timeout = timeout
 
         assert batchsize>0, "batchsize must be greater than zero"
         assert nworkers>0, "nworkers must be greater than zero"
@@ -36,7 +42,7 @@ class Farm:
             x = sum(func(x), [])
         return x
 
-    def run(self, iterable_input):
+    def map(self, iterable_input):
 
         iterable = grouper(tqdm.tqdm(iterable_input, desc="farm input", disable=not self.show_progress_bar), self.batchsize)
         
@@ -45,7 +51,104 @@ class Farm:
                 yield y
         else:
 
+            results_handlers: List[AsyncResult] = []
             with mp.Pool (self.nworkers) as pool:
 
-                for y in pool.imap (self.parallel_process, iterable):
-                    yield y
+                # get nworkers batches from the input iterable, submit them to the pool 
+                for _,task in zip(range (self.nworkers), iterable):
+                    handle = pool.apply_async ( self.parallel_process, [task] )
+                    results_handlers.append (handle)
+                
+                # iterate the remaining batches, get one batch at a time and submit it when a previous task is completed
+                for task in iterable:
+                    i=0
+                    do_next_task = False
+                    while not do_next_task:
+                        handle = results_handlers[i]
+                        try:
+                            y = handle.get ( self.timeout )
+                            del results_handlers[i]
+                            h = pool.apply_async (self.parallel_process, [task])
+                            results_handlers.append (h)
+                            yield y
+                            do_next_task = True
+
+                        except TimeoutError:
+                            i = (i+1) % len(results_handlers)
+
+                i = 0
+                # wait until all the tasks are completed
+                while len(results_handlers):
+                    handle = results_handlers[i]
+                    try:
+                        y = handle.get ( self.timeout )
+                        del results_handlers[i]
+                        yield y
+                        i = 0
+
+                    except TimeoutError:
+                        i = (i+1) % len(results_handlers)
+
+    def map_reduce (self, iterable_input, reduce_fn, reduce_batch):
+
+        iterable = grouper(tqdm.tqdm(iterable_input, desc="farm input", disable=not self.show_progress_bar), self.batchsize)
+        
+        if self.nworkers == 1:
+            first = True
+            out = None
+            for y in grouper (map (self.parallel_process, iterable), reduce_batch):
+                y = sum (y, [])
+                out = reduce_fn ( y + ([] if first else [out]) )
+                first = False
+            return out
+        else:
+
+            partial_reduce = functools.partial (wrap_function_result_inside_a_list, reduce_fn)
+            cached_results = []
+            results_handlers: List[AsyncResult] = []
+            with mp.Pool (self.nworkers) as pool:
+
+                # get nworkers batches from the input iterable, submit them to the pool 
+                for _,task in zip(range (self.nworkers), iterable):
+                    handle = pool.apply_async ( self.parallel_process, [task] )
+                    results_handlers.append (handle)
+                
+                # iterate the remaining batches, get one batch at a time and submit it when a previous task is completed
+                for task in iterable:
+                    i=0
+                    do_next_task = False
+                    while not do_next_task:
+                        handle = results_handlers[i]
+                        try:
+                            y = handle.get ( self.timeout )
+                            del results_handlers[i]
+                            h = pool.apply_async (self.parallel_process, [task])
+                            results_handlers.append (h)
+                            cached_results.extend (y)
+                            do_next_task = True
+
+                        except TimeoutError:
+                            i = (i+1) % len(results_handlers)
+                    
+                    if len(cached_results) >= reduce_batch:
+                        h = pool.apply_async (partial_reduce, [cached_results])
+                        results_handlers.append (h)
+                        cached_results = []                      
+
+                i = 0
+                # wait until all the tasks are completed
+                while len(results_handlers):
+                    handle = results_handlers[i]
+                    try:
+                        y = handle.get ( self.timeout )
+                        del results_handlers[i]
+                        cached_results.extend (y)
+                        i = 0
+
+                    except TimeoutError:
+                        i = (i+1) % len(results_handlers)
+                
+                return reduce_fn (cached_results)
+
+
+
--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import unittest
 import random
 import os
 import time
-import Pipeline
 import Farm
 
 def double(x_list):
@@ -22,75 +21,68 @@ def increment(x_list):
         # print(os.getpid(), time.time(), "finished increment", x)
         yield [x + 1]
 
-class State:
-    def __init__(self):
-        self.received_items=0
-        self.sum_items = 0
-
-    def fun(self, x_list):
-        for x in x_list:
-            self.received_items += 1
-            # print ("[STATE][{}] received new item {}. total items received {}".format(os.getpid(), x, self.received_items))
-            self.sum_items += x
-            yield [self.sum_items]
-
-@unittest.skip("")
-class TestPipeline(unittest.TestCase):
-
-    def test_simple(self):
-        pipeline = Pipeline.Pipeline([double, increment, double], [1, 1, 1])
-
-        for y, t in zip (pipeline.run(range(12)), [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46]):
-            self.assertEqual(y[0], t, "pipeline does not work")
-
-    def test_state(self):
-
-        state_class = State()
-        pipeline = Pipeline.Pipeline([double, increment, double, state_class.fun], [1, 1, 1, 1])
-
-        for y, t in zip (pipeline.run(range(12)), [2, 8, 18, 32, 50, 72, 98, 128, 162, 200, 242, 288]):
-            self.assertEqual(y[0], t, "pipeline does not work")
-
-    def test_batch_size(self):
-        pipeline = Pipeline.Pipeline([double, increment, double], [1, 1, 1], [3,3,2,1])
-
-        for y, t in zip (pipeline.run(range(12)), [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46]):
-            self.assertEqual(y[0], t, "pipeline does not work")
-
+def expensive_sum (x_list):
+    out = 0
+    for x in x_list:
+        for i in range(random.randint(700000, 1300000)):
+            pass
+        out += x
+    return out
 
 class TestFarm(unittest.TestCase):
 
     def test_simple(self):
         farm = Farm.Farm([double, increment, double], 1, show_progress_bar = False)
-
-        for y, t in zip (farm.run(range(12)), [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46]):
-            self.assertEqual(y[0], t, "farm does not work")
-
-    def test_state(self):
-
-        state_class = State()
-        farm = Farm.Farm([double, increment, double, state_class.fun], 1, show_progress_bar = False)
-
-        for y, t in zip (farm.run(range(12)), [2, 8, 18, 32, 50, 72, 98, 128, 162, 200, 242, 288]):
-            self.assertEqual(y[0], t, "farm does not work")
+        exp_results = {2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46}
+        results = set ( y[0] for y in farm.map (range(12)) )
+        self.assertEqual (results, exp_results, "farm does not work")       
 
     def test_batch_size(self):
         farm = Farm.Farm([double, increment, double], 1, 2, show_progress_bar = False)
 
-        ys = sum (farm.run(range(12)), [])
+        ys = sum (farm.map(range(12)), [])
         ts = [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46]
 
         self.assertEqual (ys, ts, "farm does not work")
 
-        for y in farm.run (range(12)):
+        for y in farm.map (range(12)):
             self.assertEqual (len(y), 2, "farm with batchsize=2 does not return 2 results")
 
     def test_workers(self):
         farm = Farm.Farm([double, increment, double], 2, show_progress_bar = False)
+        exp_results = {2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46}
+        results = set ( y[0] for y in farm.map (range(12)) )
+        self.assertEqual (results, exp_results, "farm does not work")
 
-        for y, t in zip (farm.run(range(12)), [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46]):
-            self.assertEqual(y[0], t, "farm does not work")
+    def test_batch_size_and_workers(self):
+        farm = Farm.Farm([double, increment, double], 2, 2, show_progress_bar = False)
 
+        ys = set(sum (farm.map(range(12)), []))
+        ts = {2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46}
+
+        self.assertEqual (ys, ts, "farm does not work")
+
+        for y in farm.map (range(12)):
+            self.assertEqual (len(y), 2, "farm with batchsize=2 does not return 2 results")
+
+    def test_map_reduce (self):
+        farm = Farm.Farm([double, increment, double], 1, show_progress_bar = False)
+        exp_result = 288
+        result = farm.map_reduce (range(12), expensive_sum, 5)
+        self.assertEqual (result, exp_result, "map reduce does not work")       
+
+    def test_map_reduce_workers (self):
+        farm = Farm.Farm([double, increment, double], 2, show_progress_bar = False)
+        exp_result = 288
+        result = farm.map_reduce (range(12), expensive_sum, 3)
+        self.assertEqual (result, exp_result, "map reduce does not work")       
+       
+    def test_map_reduce_workers_and_batchsize (self):
+        farm = Farm.Farm([double, increment, double], 2, 2, show_progress_bar = False)
+        exp_result = 288
+        result = farm.map_reduce (range(12), expensive_sum, 3)
+        self.assertEqual (result, exp_result, "map reduce does not work")       
+       
+        
 if __name__ == "__main__":
-    unittest.main()
-    
+    unittest.main()    
