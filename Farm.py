7eb3fd85e91e89263bbe4bc1c34b1688db7f5223
Lucio Messina <luciotuttominuscolo@inventati.org>
2020-07-30 13:33:03 +0200

    documentation for Farm

--- Farm.py
+++ Farm.py
@@ -24,13 +24,51 @@ def wrap_function_result_inside_a_list ( function, input ):
     return [function(input)]
 
 class Farm:
+    '''
+     Helper class to compute composition of functions over an input iterable, using multiprocessing for heavy computations.
+     Implements the map and map-reduce paradigms. 
 
-    def __init__(self, list_of_functions, nworkers, batchsize=1, show_progress_bar=True, timeout=0.001):
+     `function composition`
+     The composition of a list of functions f_1, f_2, ... f_m is a function g defined as g(x) := f_1 (f_2 ( ... f_m (x) ) )
+     
+     `map paradigm`
+     An iterable input X = {x_1, x_2 ... x_n} can be mapped using a function g resulting into an iterable Y := {g(x_1), g(x_2), ..., g(x_n)}
+     This works just like the map builtin function: https://docs.python.org/3/library/functions.html#map
+
+     `reduce paradigm`
+     An iterable Y = {y_1, y_2, ..., y_n} can be reduced using a function h resulting in h( h( h( ..., h( h(y_1, y_2), y_3 ), ..., y_n )))
+     This works just like functools.reduce https://docs.python.org/3/library/functools.html#functools.reduce
+     if h is associative and commutative it can be applied in any order to the elements y_i
+
+     `map-reduce paradigm`
+     the map-reduce paradigm simply consist into mapping an input iterable using a function g and then reducing the result using another function h.
+
+     This class implements the map-reduce paradigm with some variants:
+      1. the function g used for the map phase is the composition of a list of functions f_1, f_2, ... f_m
+      2. each function f_i is actually a python generator that accepts a list of inputs [x_1, ... x_b] and yields zero or more lists of results to be processed by f_i+1
+      3. the reduction function h must be commutative and associative.
+      4. h accepts a list of inputs [y_1, ... y_r] and returns the reducted result. In this way only approx. r partial results are kept in memory at any given moment rather than the whole iterable Y.
+
+    b (the batch size for the map phase) and r (the batch size for the reduction phase) are parameters that must be fine-tuned in order to improve the performances when using multiprocessing
+     
+    '''
+
+    def __init__(self, list_of_functions, nworkers, batchsize=1, show_progress_bar=True):
+        '''
+         initializes a Farm object.
+
+         :param: list_of_functions generators f_1, ... f_n to be composed. See the documentation for the class Farm for details.
+         :param: nworkers maximum number of processes to be spawn for both the map and the reduce phase.
+         :param: batchsize number of input elements to be given in input to f_1. The input is chunked only when nworkers>1
+         :param: show_progress_bar whether to show a progress bar to monitor the number of input elements processed by map() and map_reduce() 
+
+        '''
+
         self.functions = list_of_functions
         self.nworkers = nworkers
         self.show_progress_bar = show_progress_bar
         self.batchsize = batchsize
-        self.timeout = timeout
+        self.timeout = 0.001
 
         assert batchsize>0, "batchsize must be greater than zero"
         assert nworkers>0, "nworkers must be greater than zero"
@@ -58,6 +96,14 @@ class Farm:
         return y
 
     def map(self, iterable_input):
+        '''
+         applies the functions composition to each element of an input iterable. See the documentation for the class Farm for details.
+
+         Yelds g(x_1), g(x_2) ... g(x_n) where g is the composition of the functions specified in the constructor and X is the input iterable.
+         The results are not yielded in order when nworkers > 1
+
+         :param: iterable_input the iterable to map
+        '''
 
         iterable = grouper(tqdm.tqdm(iterable_input, desc="farm input", disable=not self.show_progress_bar), self.batchsize)
         
@@ -86,6 +132,14 @@ class Farm:
                     yield y
 
     def map_reduce (self, iterable_input, reduce_fn, reduce_batch):
+        '''
+         applies the functions composition to each element of an input iterable, then reduces the results using another function.
+         See the documentation for the class Farm for details.
+
+         :param: iterable_input the iterable to map
+         :param: reduce_fn functions that reduces mapped results. It must be associative and commutative
+         :param: reduce_batch number of map results to compute before each call to reduce_fn
+        '''
 
         iterable = grouper(tqdm.tqdm(iterable_input, desc="farm input", disable=not self.show_progress_bar), self.batchsize)
         
